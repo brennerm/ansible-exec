8f10a6c423f8d65e95be870f92b1410ff1f10cb1
brennerm <xamrennerb@gmail.com>
2017-03-27 21:01:57 +0200

    increase version 1.3

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='ansible-runner',
     scripts=['ansible-runner'],
     install_requires=['enum34', 'pyaml'],
-    version='1.2',
+    version='1.3',
     description='Runs your playbooks, roles and tasks',
     author='Max Brenner',
     author_email='xamrennerb@gmail.com',
     url='https://github.com/brennerm/ansible-runner',
-    download_url='https://github.com/brennerm/ansible-runner/archive/1.2.tar.gz',
+    download_url='https://github.com/brennerm/ansible-runner/archive/1.3.tar.gz',
     keywords=['ansible', 'playbook', 'role', 'task']
 )
