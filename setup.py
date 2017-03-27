25334ba553777cb0188b0de5e81196c058a513e3
brennerm <xamrennerb@gmail.com>
2017-03-27 20:22:47 +0200

    increase version to 1.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='ansible-runner',
     scripts=['ansible-runner'],
-    version='1.0',
+    version='1.1',
     description='Runs your playbooks, roles and tasks',
     author='Max Brenner',
     author_email='xamrennerb@gmail.com',
     url='https://github.com/brennerm/ansible-runner',
-    download_url='https://github.com/brennerm/ansible-runner/archive/1.0.tar.gz',
+    download_url='https://github.com/brennerm/ansible-runner/archive/1.1.tar.gz',
     keywords=['ansible', 'playbook', 'role', 'task']
 )
